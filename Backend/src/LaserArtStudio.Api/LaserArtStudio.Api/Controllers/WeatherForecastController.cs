589126c912e4b08dab8e95ea21651f9b941b0980
Miguel López <miguellopezo1107@gmail.com>
2026-06-29 15:03:34 -0600

    Backend and frontend configuration repo

--- Backend/src/LaserArtStudio.Api/LaserArtStudio.Api/Controllers/WeatherForecastController.cs
+++ Backend/src/LaserArtStudio.Api/LaserArtStudio.Api/Controllers/WeatherForecastController.cs
@@ -1,17 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace LaserArt_Studio.Controllers
+namespace LaserArtStudio.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries =
-        [
+        private static readonly string[] Summaries = new[]
+        {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
+        };
 
-        [HttpGet]
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
